ec7191ab72e26780c4d10d93dca7f35eb87f647e
Joris Baiutti <joris.baiutti@gmail.com>
2018-10-28 15:25:58 +0100

    added EntityFramework forAuthentication

--- MQTTalk.App/Startup.cs
+++ MQTTalk.App/Startup.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using MQTTalk.App.Data;
 using MQTTalk.App.Signaling;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace MQTTalk.App
 {
@@ -31,6 +39,40 @@ namespace MQTTalk.App
 
             services.AddSignalR();
 
+            services.AddDbContext<UserDbContext>(options =>
+            {
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=UserDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<UserDbContext>()
+                .AddDefaultTokenProviders();
+
+            // ===== Add Jwt Authentication ========
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["https://mqttalk.mobilegees.com"],
+                        ValidAudience = Configuration["https://mqttalk.mobilegees.com"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("fc1d4f60-f18b-4bfc-829b-103e7d2f692c")),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                });
+
+
+
             services.AddCors(options => options.AddPolicy("CorsPolicy",
             builder =>
             {
@@ -39,7 +81,7 @@ namespace MQTTalk.App
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, UserDbContext userDbContext)
         {
             if (env.IsDevelopment())
             {
@@ -56,10 +98,12 @@ namespace MQTTalk.App
             app.UseSpaStaticFiles();
 
             app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseSignalR(routes =>
             {
                 routes.MapHub<WebRtcHub>("/webRtcHub");
             });
+
 
             app.UseMvc(routes =>
             {
@@ -67,6 +111,7 @@ namespace MQTTalk.App
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
+            
 
             app.UseSpa(spa =>
             {
@@ -80,6 +125,8 @@ namespace MQTTalk.App
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+
+            userDbContext.Database.EnsureCreated();
         }
     }
 }
