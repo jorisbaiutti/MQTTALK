b63b7dbf23cd6914815555a9a07a7d945ac0af9d
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2018-11-20 20:04:43 +0100

    Split angular app from web api

--- Server/MQTTalk.App/Startup.cs
+++ Server/MQTTalk.App/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
@@ -15,7 +14,6 @@ using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using System.Threading.Tasks;
-using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace MQTTalk.App
 {
@@ -33,12 +31,6 @@ namespace MQTTalk.App
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
 
             services.AddSignalR();
 
@@ -114,41 +106,17 @@ namespace MQTTalk.App
             }
             else
             {
-                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
+            app.UseMvc();
 
             app.UseCors("CorsPolicy");
             app.UseAuthentication();
             app.UseSignalR(routes =>
             {
                 routes.MapHub<WebRtcHub>("/hub/webRtcHub");
-            });
-
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
-
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
             });
 
             userDbContext.Database.EnsureCreated();
