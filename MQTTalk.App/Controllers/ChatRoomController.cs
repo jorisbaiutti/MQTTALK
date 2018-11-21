a14fa31df8688124cac03b887ee1757f2f75c223
Joris <joris.baiutti@gmail.com>
2018-11-21 19:14:18 +0100

    implemented getting chats in controller

--- MQTTalk.App/Controllers/ChatRoomController.cs
+++ MQTTalk.App/Controllers/ChatRoomController.cs
@@ -11,24 +11,69 @@ namespace MQTTalk.App.Controllers
     [ApiController]
     public class ChatRoomController : ControllerBase
     {
-        public ChatRoomController()
+        private IChatRepository _chatRepository;
+
+        public ChatRoomController(IChatRepository chatRepository)
         {
-            
+            _chatRepository = chatRepository;
         }
         [HttpGet]
         [Authorize]
         public ActionResult<List<ChatRoomDto>> GetChatRooms()
         {
+            var chats = _chatRepository.GetChats().Select(c => new ChatRoomDto()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                OwnerUserId = c.OwnerUserId,
+                Description = c.Description,
+                MemberUserList = c.MemberUserList.Select(u => new UserDto()
+                {
+                    Id = u.UserId,
+                    Email = u.EMail,
+                    online = true
+                }).ToList(),
+                MessageList = c.MessageList.Select(m => new MessageDto()
+                {
+                    Id = m.Id,
+                    Sent = m.Sent,
+                    FromUserId = m.FromUserId,
+                    ToUserId = m.ToUserId
 
-            return Ok("");
+                }).ToList()
+            }).ToList();
+            return Ok(chats);
         }
-  
+
 
         [HttpGet("{id}")]
         [Authorize]
         public ActionResult<ChatRoomDto> GetChatRoom(int id)
         {
-            return Ok("");
+            var chatRoom = _chatRepository.GetChat(id);
+            var chatRoomDto = new ChatRoomDto()
+            {
+                Id = chatRoom.Id,
+                Name = chatRoom.Name,
+                OwnerUserId = chatRoom.OwnerUserId,
+                Description = chatRoom.Description,
+                MemberUserList = chatRoom.MemberUserList.Select(u => new UserDto()
+                {
+                    Id = u.UserId,
+                    Email = u.EMail,
+                    online = true
+                }).ToList(),
+                MessageList = chatRoom.MessageList.Select(m => new MessageDto()
+                {
+                    Id = m.Id,
+                    Sent = m.Sent,
+                    FromUserId = m.FromUserId,
+                    ToUserId = m.ToUserId
+
+                }).ToList()
+            };
+
+            return Ok(chatRoomDto);
         }
 
         [HttpPost]
@@ -40,7 +85,8 @@ namespace MQTTalk.App.Controllers
 
         [HttpDelete("{id}")]
         [Authorize]
-        public ActionResult<ChatRoomDto> DeleteChatRoom(int id){
+        public ActionResult<ChatRoomDto> DeleteChatRoom(int id)
+        {
             return Ok("");
         }
 
